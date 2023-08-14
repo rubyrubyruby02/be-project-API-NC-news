e105e48e489082e5183278de025f36ea438aff90
Ruby <ruby.porter@tiscali.co.uk>
2023-08-14 14:13:21 +0100

    removal of console.log before review

--- __tests__/Q2_topics.tests.js
+++ __tests__/Q2_topics.tests.js
@@ -21,11 +21,7 @@ describe('Adds first endpoint /api/topics', ()=> {
         .get('/api/topics')
         .expect(200)
         .then((result) => {
-
-            console.log(result.body)
-
             expect((result.body.topics)).toHaveLength(3)
-
             result.body.topics.forEach((topic)=>{
                 expect(topic).toHaveProperty('slug', expect.any((String)))
                 expect(topic).toHaveProperty('description', expect.any((String)))
