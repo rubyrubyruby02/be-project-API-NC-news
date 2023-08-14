b9f27c022415cd8420cf229d8c3d2c2d5497f9b5
Ruby <ruby.porter@tiscali.co.uk>
2023-08-14 15:50:22 +0100

    removes model to readFile in controller, adds test to compare raw data to actual data by importing in datafile

--- __tests__/Q3_available_endpoints.test.js
+++ __tests__/Q3_available_endpoints.test.js
@@ -3,6 +3,7 @@ const db = require('../db/connection')
 const seed = require('../db/seeds/seed')
 const request = require('supertest')
 const testData=require('../db/data/test-data/index')
+const endpointData = require('../endpoints.json')
 
 
 beforeEach(()=> {
@@ -15,7 +16,19 @@ afterAll(()=> {
 
 
 describe('Endpoint /api returns a list with a description of all available endpoints ', ()=> {
-    test('Status 200: /api returns endpoint object with properties of description, queries, exampleResponse and articles', ()=> {
+    test('Status 200: tests entire file by comparing raw data to returned actual data', ()=> {
+
+        return request(app)
+        .get('/api')
+        .expect(200)
+        .then((response)=> {
+            const result = response.body.result
+            const expectedDataFromJsonFile = JSON.stringify(endpointData)
+            const actualData = JSON.stringify(result)
+            expect(actualData).toEqual(expectedDataFromJsonFile)
+        })
+    })
+    test('Status 200: /api returns object with properties of description, queries, exampleResponse and articles', ()=> {
         return request(app)
         .get('/api')
         .expect(200)
@@ -23,7 +36,7 @@ describe('Endpoint /api returns a list with a description of all available endpo
 
             const result = response.body.result
             const endpoints = result
-            
+
             for(const endpointKey in endpoints){
                 const endpoint = endpoints[endpointKey]
                 expect(endpoint).toHaveProperty('description')
--- controllers/endpoints.controller.js
+++ controllers/endpoints.controller.js
@@ -1,9 +1,20 @@
-const {fetchEndpoints} = require('../models/endpoints.model')
+const fs = require('fs/promises')
 
 const getEndpoints = (request, response, next)=> {
-
-    fetchEndpoints().then((result)=> {
+    fetchEndpoints()
+    .then((result)=> {
         response.status(200).send({result})
     })
+    .catch(next)
 }
+
+const fetchEndpoints = ()=> {
+    return fs.readFile('./endpoints.json', 'utf-8')
+    .then((endpointsList) => {
+        const parsedDataOfEndpoints = JSON.parse(endpointsList)
+        return parsedDataOfEndpoints
+    })
+
+}
+
 module.exports = {getEndpoints}
