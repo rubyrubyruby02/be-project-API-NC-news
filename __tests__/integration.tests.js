9830f4dd26d21e3c633b34c19bb7778f89a9c034
Ruby <ruby.porter@tiscali.co.uk>
2023-08-15 14:23:57 +0100

    WIP Q5 toBeSortedBy

--- __tests__/integration.tests.js
+++ __tests__/integration.tests.js
@@ -68,7 +68,69 @@ describe('Q3 Endpoint /api returns a list with a description of all available en
         })
     })
 })
-describe.skip('Q5  adds /api/articles and returns articles in data-descending order', ()=> {
+
+describe('Q4 GET /api/articles/:article_id', ()=> {
+    test('Status 200: returns article object with properties of author, title, article_id, body, topic, created_at, votes, article_img_url', ()=> {
+
+        return request(app)
+        .get('/api/articles/2')
+        .expect(200)
+        .then((response) => {
+
+            const result = response.body.result
+
+                expect(result).toHaveProperty('author', expect.any((String)))
+                expect(result).toHaveProperty('title', expect.any((String)))
+                expect(result).toHaveProperty('article_id', expect.any((Number)))
+                expect(result).toHaveProperty('body', expect.any((String)))
+                expect(result).toHaveProperty('topic', expect.any((String)))
+                expect(result).toHaveProperty('created_at', expect.any((String)))
+                expect(result).toHaveProperty('votes', expect.any((Number)))
+                expect(result).toHaveProperty('article_img_url', expect.any((String)))
+            
+        })
+    })
+    test('Status 200, /api/articles/1 - checks expected output for first article', ()=> {
+        return request(app)
+        .get('/api/articles/1')
+        .expect(200)
+        .then((response)=> {
+
+            const returnedData = {
+                article_id: 1,
+                title: 'Living in the shadow of a great man',
+                topic: 'mitch',
+                author: 'butter_bridge',
+                body: 'I find this existence challenging',
+                created_at: "2020-07-09T20:11:00.000Z",
+                votes: 100,
+                article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700'
+              }
+
+            expect(response.body).toEqual({result: returnedData})
+            
+        })
+    })
+    test('Status 400: not found, custom message when path is invalid (e.g. article_id is input a a string)', ()=> {
+        return request(app)
+        .get('/api/articles/not_a_number')
+        .expect(400)
+        .then((response) => {
+            expect(response.body.msg).toBe("Bad request")
+        })
+    })
+    test('Status 404: a valid article_id is input but this article_id is not in the database', ()=> {
+        return request(app)
+        .get('/api/articles/999')
+        .expect(404)
+        .then((response)=> {
+            expect(response.body.msg).toBe("Article Id not found")
+        })
+    })
+})
+
+describe('Q5  adds /api/articles and returns articles in data-descending order', ()=> {
+    
     test('Status 200: returns array of article objects with properties of author, title, article_id, topic, created_at, votes, article_img_url, comment_count', ()=> {
         return request(app)
         .get('/api/articles')
@@ -83,7 +145,7 @@ describe.skip('Q5  adds /api/articles and returns articles in data-descending or
                 expect(article).toHaveProperty('created_at', expect.any((String)))
                 expect(article).toHaveProperty('votes', expect.any((Number)))
                 expect(article).toHaveProperty('article_img_url', expect.any((String)))
-                expect(article).toHaveProperty('comment_count', expect.any((Number)))
+                expect(article).toHaveProperty('comment_count', expect.any((String)))
             })
         })
     })
@@ -91,8 +153,19 @@ describe.skip('Q5  adds /api/articles and returns articles in data-descending or
         return request(app)
         .get('/api/articles')
         .expect(200)
+        .then(({body : {articles}})=> {
+            console.log(articles, "in test file")
+            expect(articles).toBeSortedBy('created_at', {descending: true})
+        })
+    })
+    test('Status 404: incorrect endpoint input', ()=> {
+        return request(app)
+        .get('/api/wrong_input')
+        .expect(404)
         .then((response)=> {
-            expect(response.body.articles).toBeSortedBy('created_at', {descending: true})
+
+            expect(response.body.msg).toBe("Not found")
+
         })
     })
 })
--- errors/errors.js
+++ errors/errors.js
@@ -1,5 +1,16 @@
-const customErrorHandler = ((error, request, response, next)=> {
-    
+const PSQLErrorHandler = ((error, request, response, next) => {
+
+    if(error.code === '22P02'){
+        response.status(400).send({
+            msg: 'Bad request'
+        })
+    }
+    else{
+        next(error)
+    }
+})
+
+const customErrorHandler = ((error, request, response, next) => {
     if(error.status && error.msg){
         response.status(error.status).send({ msg: error.msg})
     }
@@ -9,4 +20,4 @@ const customErrorHandler = ((error, request, response, next)=> {
 })
 
 
-module.exports = {customErrorHandler}
+module.exports = {customErrorHandler, PSQLErrorHandler}
