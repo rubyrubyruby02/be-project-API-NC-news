7e25e1532c9fd8525e847fe682339d0c0dc3d59c
Ruby <ruby.porter@tiscali.co.uk>
2023-08-18 13:54:15 +0100

    adds /:username to find one user

--- models/users.model.js
+++ models/users.model.js
@@ -8,4 +8,19 @@ const fetchUsers = () => {
     })
 }
 
-module.exports = {fetchUsers}
+const fetchUserByUsername = (username)=> {
+    return db.query(`SELECT * FROM users WHERE username = $1`, [username])
+    .then((result)=> {
+
+        if(result.rows.length === 0){
+            return Promise.reject({
+                status: 404,
+                msg: "Username not found"
+            })
+        }
+        return result.rows[0]
+    })
+
+
+}
+module.exports = {fetchUsers, fetchUserByUsername}
