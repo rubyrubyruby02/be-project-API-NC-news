d0e1297ad9a9bd01319e862435b4225e1bb7e4b2
Ruby <ruby.porter@tiscali.co.uk>
2023-08-18 08:54:09 +0100

    adds comment count when searching for an article by id

--- __tests__/integration.tests.js
+++ __tests__/integration.tests.js
@@ -82,35 +82,14 @@ describe('Q4 GET /api/articles/:article_id', ()=> {
 
             const result = response.body.result
 
-                expect(result).toHaveProperty('author', expect.any((String)))
-                expect(result).toHaveProperty('title', expect.any((String)))
-                expect(result).toHaveProperty('article_id', expect.any((Number)))
-                expect(result).toHaveProperty('body', expect.any((String)))
-                expect(result).toHaveProperty('topic', expect.any((String)))
-                expect(result).toHaveProperty('created_at', expect.any((String)))
-                expect(result).toHaveProperty('votes', expect.any((Number)))
-                expect(result).toHaveProperty('article_img_url', expect.any((String)))
-            
-        })
-    })
-    test('Status 200, /api/articles/1 - checks expected output for first article', ()=> {
-        return request(app)
-        .get('/api/articles/1')
-        .expect(200)
-        .then((response)=> {
-
-            const returnedData = {
-                article_id: 1,
-                title: 'Living in the shadow of a great man',
-                topic: 'mitch',
-                author: 'butter_bridge',
-                body: 'I find this existence challenging',
-                created_at: "2020-07-09T20:11:00.000Z",
-                votes: 100,
-                article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700'
-              }
-
-            expect(response.body).toEqual({result: returnedData})
+                expect(result).toHaveProperty('author', "icellusedkars")
+                expect(result).toHaveProperty('title', "Sony Vaio; or, The Laptop")
+                expect(result).toHaveProperty('article_id', 2)
+                expect(result).toHaveProperty('body', "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.")
+                expect(result).toHaveProperty('topic', "mitch")
+                expect(result).toHaveProperty('created_at', "2020-10-16T05:03:00.000Z")
+                expect(result).toHaveProperty('votes', 0)
+                expect(result).toHaveProperty('article_img_url', "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700")
             
         })
     })
@@ -475,4 +454,39 @@ describe('Q10 GET /api/users', ()=> {
             })
         })
     })
+})
+
+
+describe('Q12 QUERY GET /api/articles/:article_id (comment count)', ()=> {
+    test('Status 200: returns article object including new property of comment_count', ()=> {
+        return request(app)
+        .get('/api/articles/1')
+        .expect(200)
+        .then((response)=> {
+
+            const returnedData = {
+                article_id: 1,
+                comment_count: '11',
+                title: 'Living in the shadow of a great man',
+                topic: 'mitch',
+                author: 'butter_bridge',
+                body: 'I find this existence challenging',
+                created_at: "2020-07-09T20:11:00.000Z",
+                votes: 100,
+                article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700'
+              }
+
+            expect(response.body).toEqual({result: returnedData})
+            expect(response.body.result).toHaveProperty('comment_count', '11')
+        })
+    })
+    test('Status 200: returns when there are 0 comments', ()=> {
+        return request(app)
+        .get('/api/articles/4')
+        .expect(200)
+        .then((response)=> {
+            expect(response.body.result).toHaveProperty('comment_count', '0')
+        })
+    })
+   
 })
